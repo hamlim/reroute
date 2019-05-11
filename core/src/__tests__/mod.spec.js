e8202092b362f18ab28d3385a673bd64ff3e53a5
Matt Hamlin <hamlim@outlook.com>
2019-05-11 16:36:22 -0400

    Fix formatting

--- core/src/__tests__/mod.spec.js
+++ core/src/__tests__/mod.spec.js
@@ -1,159 +1,159 @@
-import React from "react";
-import { render, cleanup } from "react-testing-library";
+import React from 'react'
+import { render, cleanup } from 'react-testing-library'
 
-import { useHistory, useLink, useRoute, Router } from "../mod.js";
-import { createMemoryHistory } from "history";
+import { useHistory, useLink, useRoute, Router } from '../mod.js'
+import { createMemoryHistory } from 'history'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-test("it gives you the history", () => {
-  let hist;
+test('it gives you the history', () => {
+  let hist
   function Foo() {
-    hist = useHistory();
-    return null;
+    hist = useHistory()
+    return null
   }
 
   render(
     <Router createHistory={createMemoryHistory}>
       <Foo />
-    </Router>
-  );
+    </Router>,
+  )
 
-  expect(hist.location.pathname).toEqual("/");
-});
+  expect(hist.location.pathname).toEqual('/')
+})
 
-test("useLink provides a function to get link props", () => {
-  let func;
+test('useLink provides a function to get link props', () => {
+  let func
   function Foo() {
-    func = useLink("/path");
-    return null;
+    func = useLink('/path')
+    return null
   }
   render(
     <Router createHistory={createMemoryHistory}>
       <Foo />
-    </Router>
-  );
-  expect(typeof func).toBe("function");
-});
+    </Router>,
+  )
+  expect(typeof func).toBe('function')
+})
 
-test("calling the result of useLink provides the correct role", () => {
+test('calling the result of useLink provides the correct role', () => {
   function Foo() {
-    let getLinkProps = useLink("/foo");
+    let getLinkProps = useLink('/foo')
 
     return (
       <div {...getLinkProps()} data-testid="link">
         Yo
       </div>
-    );
+    )
   }
 
   let { queryByTestId } = render(
     <Router createHistory={createMemoryHistory}>
       <Foo />
-    </Router>
-  );
+    </Router>,
+  )
 
-  expect(queryByTestId("link").getAttribute("role")).toEqual("anchor");
-});
+  expect(queryByTestId('link').getAttribute('role')).toEqual('anchor')
+})
 
-test("useLink supports disabled links", () => {
+test('useLink supports disabled links', () => {
   function Foo() {
-    let getLinkProps = useLink("/foo");
+    let getLinkProps = useLink('/foo')
 
     return (
       <div {...getLinkProps({ disabled: true })} data-testid="link">
         Yo
       </div>
-    );
+    )
   }
 
   let { queryByTestId } = render(
     <Router createHistory={createMemoryHistory}>
       <Foo />
-    </Router>
-  );
+    </Router>,
+  )
 
-  expect(queryByTestId("link").getAttribute("role")).toEqual("presentation");
-});
+  expect(queryByTestId('link').getAttribute('role')).toEqual('presentation')
+})
 
-test("useRoute determines if it matches the provided path", () => {
-  let match;
+test('useRoute determines if it matches the provided path', () => {
+  let match
   function Foo() {
-    let { match: _match } = useRoute("/");
-    match = _match;
+    let { match: _match } = useRoute('/')
+    match = _match
 
-    return null;
+    return null
   }
 
   render(
     <Router createHistory={createMemoryHistory}>
       <Foo />
-    </Router>
-  );
+    </Router>,
+  )
 
-  expect(match).toBe(true);
-});
+  expect(match).toBe(true)
+})
 
-test("useRoute does not match for a different route", () => {
-  let match;
+test('useRoute does not match for a different route', () => {
+  let match
   function Foo() {
-    let { match: _match } = useRoute("/some-path-here");
+    let { match: _match } = useRoute('/some-path-here')
 
-    match = _match;
+    match = _match
 
-    return null;
+    return null
   }
   render(
     <Router createHistory={createMemoryHistory}>
       <Foo />
-    </Router>
-  );
+    </Router>,
+  )
 
-  expect(match).toBe(false);
-});
+  expect(match).toBe(false)
+})
 
 // Workaround react bug, see: https://github.com/facebook/react/issues/12485
 const pauseErrorLogging = codeToRun => {
-  const logger = console.error;
-  console.error = () => {};
+  const logger = console.error
+  console.error = () => {}
 
-  codeToRun();
+  codeToRun()
 
-  console.error = logger;
-};
+  console.error = logger
+}
 
-test("Router throws when no createHistory is provided, or its not a function", () => {
+test('Router throws when no createHistory is provided, or its not a function', () => {
   pauseErrorLogging(() =>
     expect(() => {
-      render(<Router />);
+      render(<Router />)
     }).toThrowErrorMatchingInlineSnapshot(
-      `"createHistory prop was either not provided, or is not a function."`
-    )
-  );
+      `"createHistory prop was either not provided, or is not a function."`,
+    ),
+  )
 
   pauseErrorLogging(() =>
     expect(() => {
-      render(<Router createHistory={null} />);
+      render(<Router createHistory={null} />)
     }).toThrowErrorMatchingInlineSnapshot(
-      `"createHistory prop was either not provided, or is not a function."`
-    )
-  );
-});
+      `"createHistory prop was either not provided, or is not a function."`,
+    ),
+  )
+})
 
-test("useLink`s linkClick callback throws when no history is provided within context", () => {
-  let clickHandler;
+test('useLink`s linkClick callback throws when no history is provided within context', () => {
+  let clickHandler
   function Link() {
-    let getLinkProps = useLink("/foo");
-    let { onClick, ...rest } = getLinkProps();
-    clickHandler = onClick;
-    return null;
+    let getLinkProps = useLink('/foo')
+    let { onClick, ...rest } = getLinkProps()
+    clickHandler = onClick
+    return null
   }
-  render(<Link />);
+  render(<Link />)
 
   expect(() => clickHandler({ defaultPrevented: false, preventDefault() {} }))
     .toThrowErrorMatchingInlineSnapshot(`
 "Link attempted to route to path: '/foo' but no history was found in context.
 
 Check to ensure the link is rendered within a Router."
-`);
-});
+`)
+})
